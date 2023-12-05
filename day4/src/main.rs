b70959591ca86b4ef2ba6e96f4c4f0d3f6198d48
Jay Graves <Jay@skabber.com>
2023-12-05 15:45:19 -0700

    Day4

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,11 +1,36 @@
-use std::io::BufRead;
+use std::{collections::HashSet, io::BufRead};
 
 fn main() {
     let mut val = 0;
-    for line in std::io::stdin().lock().lines() {
-        val += parse_card(line.unwrap().as_str());
+    let mut copies: Vec<usize> = Vec::new();
+
+    for (index, line) in std::io::stdin().lock().lines().enumerate() {
+        let count = parse_card(line.unwrap().as_str());
+        if copies.len() <= index {
+            copies.push(1);
+        } else {
+            copies[index] += 1;
+        }
+
+        let base: u32 = 2;
+        if count > 0 {
+            val += base.pow(count as u32 - 1);
+        }
+
+        for j in 0..count {
+            let next_index = index + 1 + j;
+            if copies.len() <= next_index {
+                copies.push(copies[index]);
+            } else {
+                copies[next_index] += copies[index];
+            }
+        }
     }
-    println!("{}", val);
+
+    let card_count = copies.iter().sum::<usize>();
+
+    println!("score: {}", val);
+    println!("card count: {}", card_count);
 }
 
 fn parse_card(line: &str) -> usize {
@@ -17,34 +42,54 @@ fn parse_card(line: &str) -> usize {
     let card_numbers = number_parts.next().unwrap();
     let wn: Vec<&str> = win_numbers.split_ascii_whitespace().collect();
     let cn: Vec<&str> = card_numbers.split_ascii_whitespace().collect();
-    // Normally use array_tool::vec::Intersect
-    let mut val = 0;
-    for x in wn {
-        for y in &cn {
-            if y == &x {
-                if val == 0 {
-                    val = 1;
-                } else {
-                    val *= 2;
-                }
-                println!("{}:{}", y, val);
-            }
-        }
-    }
-    val
+
+    let hs1: HashSet<_> = wn.into_iter().collect();
+    let hs2: HashSet<_> = cn.into_iter().collect();
+    let intersection: HashSet<_> = hs1.intersection(&hs2).collect();
+    let result_vec: Vec<_> = intersection.into_iter().cloned().collect();
+
+    result_vec.len()
 }
 
 #[test]
 fn test_cards() {
     let input = r#"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"#;
-    let mut value = 0;
-    for line in input.split('\n') {
-        value += parse_card(line);
+    Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+    Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+    Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+    Card 5: 1 2 3 4 5 | 6 7 8 9 10
+    Card 6: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+    Card 7: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"#;
+
+    let mut val = 0;
+    let mut copies: Vec<usize> = Vec::new();
+
+    for (index, line) in input.split('\n').enumerate() {
+        let count = parse_card(line);
+        if copies.len() <= index {
+            copies.push(1);
+        } else {
+            copies[index] += 1;
+        }
+
+        let base: u32 = 2;
+        if count > 0 {
+            val += base.pow(count as u32 - 1);
+        }
+
+        for j in 0..count {
+            let next_index = index + 1 + j;
+            if copies.len() <= next_index {
+                copies.push(copies[index]);
+            } else {
+                copies[next_index] += copies[index];
+            }
+        }
     }
-    assert_eq!(value, 13);
+
+    println!("{:?}", copies);
+
+    let card_count = copies.iter().sum::<usize>();
+    assert_eq!(val, 13);
+    assert_eq!(card_count, 31);
 }
