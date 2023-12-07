3902ba4d36e0407be788bad29a4446291d4004f4
Jay Graves <Jay@skabber.com>
2023-12-07 12:10:56 -0700

    day5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,37 +1,82 @@
-struct Map<'a> {
-    section: Section<'a>,
-}
+use std::cmp::Ordering;
+use std::io::Read;
+use std::ops::Range;
 
 #[derive(Debug)]
 struct Section<'a> {
     lines: Vec<&'a str>,
 }
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+struct SourceDestinationMap {
+    source_range: Range<u64>,
+    destination_range: Range<u64>,
 }
 
-fn parse_input(input: &str) -> u32 {
-    let mut lowest_location = 0;
-    let sections = get_structured_map(input);
-    for section in sections {
-        for (i, line) in section.lines.iter().enumerate() {
-            if i == 0 {
-                let seedparts = line.split(':').collect::<Vec<&str>>()[1];
-            }
+fn main() {
+    let mut input = String::new();
+    std::io::stdin().lock().read_to_string(&mut input).unwrap();
+    let val = parse_input(input.as_str());
+    println!("{}", val);
+}
 
-            // if line.split(':').collect() {}
-            if i > 0 {
-                let r_parts: Vec<&str> = line.split_ascii_whitespace().collect();
-                let start: u32 = r_parts[0].parse().unwrap();
-                let end: u32 = r_parts[1].parse().unwrap();
-                let size: u32 = r_parts[2].parse().unwrap();
-                let range_1 = start..size;
-                let range_2 = end..size;
+fn parse_input(input: &str) -> u64 {
+    let sections = get_structured_map(input);
+    let mut seed_numbers: Vec<u64> = Vec::new();
+
+    for section in sections {
+        let mut maps: Vec<SourceDestinationMap> = Vec::new();
+        let mut locations: Vec<u64> = Vec::new();
+        for (i, line) in section.lines.iter().enumerate() {
+            match i.cmp(&0) {
+                Ordering::Equal => {
+                    let seedparts: Vec<&str> = line.split(':').collect::<Vec<&str>>()[1]
+                        .split_ascii_whitespace()
+                        .collect();
+                    if !seedparts.is_empty() {
+                        seed_numbers = seedparts
+                            .iter()
+                            .map(|p| p.parse::<u64>().unwrap())
+                            .collect();
+                    }
+                }
+                Ordering::Greater => {
+                    let r_parts: Vec<&str> = line.split_ascii_whitespace().collect();
+                    let start: u64 = r_parts[0].parse().unwrap();
+                    let end: u64 = r_parts[1].parse().unwrap();
+                    let size: u64 = r_parts[2].parse().unwrap();
+                    let destination_range = start..start + size;
+                    println!("about to {}", end);
+                    let source_range: std::ops::Range<u64> = end..end + size;
+                    maps.push(SourceDestinationMap {
+                        source_range,
+                        destination_range,
+                    });
+                }
+                Ordering::Less => {}
             }
         }
+        println!("{:?}", maps);
+
+        for seed in &seed_numbers {
+            let mut dest_value_op: Option<u64> = None;
+            for map in &maps {
+                if map.source_range.contains(seed) {
+                    let source_index = seed - map.source_range.start;
+                    let dest_value = map.destination_range.start + source_index;
+                    dest_value_op = Some(dest_value);
+                    locations.push(dest_value);
+                    println!("{:?}", locations);
+                }
+            }
+            if dest_value_op.is_none() {
+                locations.push(*seed);
+            }
+        }
+        seed_numbers = locations.clone();
     }
-    lowest_location
+    seed_numbers.sort();
+    seed_numbers[0]
 }
 
 fn get_structured_map(input: &str) -> Vec<Section> {
@@ -39,11 +84,11 @@ fn get_structured_map(input: &str) -> Vec<Section> {
     let mut section: Option<Section> = None;
     for line in input.split('\n') {
         match section {
-            None => { section = Some(Section { lines: Vec::new() }) }
+            None => section = Some(Section { lines: Vec::new() }),
             Some(_) => {}
         }
 
-        if line == "" {
+        if line.is_empty() {
             sections.push(section.unwrap());
             section = None;
         } else {
@@ -97,5 +142,3 @@ humidity-to-location map:
         assert_eq!(lowest_location, 35);
     }
 }
-
-
