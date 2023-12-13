f0e417c9dc40931e73996820e24f12151b50b634
Jay Graves <Jay@skabber.com>
2023-12-12 17:01:01 -0700

    Day 8 Part 1 Had to learn LCM from other solutions.

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -5,8 +5,13 @@ fn main() {
     std::io::stdin().lock().read_to_string(&mut input).unwrap();
     let mut lines = input.lines().clone();
     let (directions, map) = parse_lines(&mut lines);
-    let count = loop_map(directions, map);
+
+    let count = loop_maps(&directions, &map, "AAA", "ZZZ");
     println!("Part 1: {}", count); //12361
+
+    let starts: Vec<&str> = map.keys().filter(|e| e.ends_with(&"A")).copied().collect();
+    let val = lcm_for_starts(starts, directions, map);
+    println!("Part 2: {}", val); // 18215611419223
 }
 
 fn parse_lines<'a>(lines: &'a mut Lines<'a>) -> (Vec<char>, HashMap<&'a str, Vec<&'a str>>) {
@@ -44,7 +49,8 @@ fn follow_map<'a>(
     directions: Vec<char>,
     map: HashMap<&'a str, Vec<&'a str>>,
     start_key: &str,
-) -> (&'a str, u64) {
+    _destination: &str,
+) -> (&'a str, usize) {
     let (mut node_key, mut node_values) = map.get_key_value(start_key).unwrap();
     let mut step_count = 0;
     for d in directions {
@@ -60,34 +66,63 @@ fn follow_map<'a>(
                 panic!("Unknown direction: {}", d);
             }
         }
-        if node_key == &"ZZZ" {
-            break;
-        }
     }
     (node_key, step_count)
 }
 
-fn loop_map<'a>(directions: Vec<char>, map: HashMap<&'a str, Vec<&'a str>>) -> u64 {
-    let mut node_key = "AAA";
-    let mut count = 0;
-    let mut loop_count = 0;
+fn loop_maps<'a>(
+    directions: &[char],
+    map: &HashMap<&'a str, Vec<&'a str>>,
+    start: &str,
+    destination: &str,
+) -> usize {
+    let mut node_key: &str = start;
+    let mut count: usize = 0;
     loop {
-        loop_count += 1;
-        let (dest, c) = follow_map(directions.clone(), map.clone(), node_key);
+        let (dest, c) = follow_map(directions.to_vec(), map.clone(), node_key, destination);
         count += c;
-        if dest == "ZZZ" {
+
+        if dest.ends_with(destination) {
             break;
         } else {
             node_key = dest;
         }
     }
-    println!("Loop count: {}", loop_count);
     count
+}
+
+fn lcm_for_starts(
+    starts: Vec<&str>,
+    directions: Vec<char>,
+    map: std::collections::HashMap<&str, Vec<&str>>,
+) -> usize {
+    let counts: Vec<usize> = starts
+        .iter()
+        .map(|n| loop_maps(&directions, &map, n, "Z"))
+        .collect();
+
+    lcm(&counts[..])
+}
+
+pub fn lcm(nums: &[usize]) -> usize {
+    if nums.len() == 1 {
+        return nums[0];
+    }
+    let a = nums[0];
+    let b = lcm(&nums[1..]);
+    a * b / gcd_of_two_numbers(a, b)
+}
+
+fn gcd_of_two_numbers(a: usize, b: usize) -> usize {
+    if b == 0 {
+        return a;
+    }
+    gcd_of_two_numbers(b, a % b)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{loop_map, parse_lines};
+    use crate::{lcm_for_starts, loop_maps, parse_lines};
 
     const INPUT: &str = "LLR
 
@@ -99,7 +134,27 @@ ZZZ = (ZZZ, ZZZ)";
     fn test_part1() {
         let mut lines = INPUT.lines();
         let (directions, map) = parse_lines(&mut lines);
-        let count = loop_map(directions, map);
-        assert_eq!(count, 6_u64);
+
+        let count = loop_maps(&directions, &map, "AAA", "ZZZ");
+        assert_eq!(count, 6_usize);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = "LR
+
+        11A = (11B, XXX)
+        11B = (XXX, 11Z)
+        11Z = (11B, XXX)
+        22A = (22B, XXX)
+        22B = (22C, 22C)
+        22C = (22Z, 22Z)
+        22Z = (22B, 22B)
+        XXX = (XXX, XXX)";
+        let mut lines = input.lines();
+        let (directions, map) = parse_lines(&mut lines);
+        let starts: Vec<&str> = map.keys().filter(|e| e.ends_with(&"A")).copied().collect();
+        let val = lcm_for_starts(starts, directions, map);
+        assert_eq!(val, 6);
     }
 }
