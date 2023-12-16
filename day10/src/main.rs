ebf8f18f5bfd0ba09d74376a649fa76adf1c56d1
Jay Graves <Jay@skabber.com>
2023-12-15 19:49:53 -0700

    Day 10 Part 1

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,11 +1,12 @@
 use std::{char, io::Read, str::Lines};
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum Direction {
     North,
     East,
     South,
     West,
+    Start,
     None,
 }
 
@@ -17,16 +18,43 @@ struct Point {
 }
 
 #[derive(Debug, Clone, Copy)]
-struct Glyph(Direction, Direction, char);
+struct Glyph {
+    directions: (Direction, Direction),
+    c: char,
+}
 
-const NORTH_SOUTH: Glyph = Glyph(Direction::North, Direction::South, '|');
-const EAST_WEST: Glyph = Glyph(Direction::East, Direction::West, '-');
-const NORTH_EAST: Glyph = Glyph(Direction::North, Direction::East, 'L');
-const NORTH_WEST: Glyph = Glyph(Direction::North, Direction::West, 'J');
-const SOUTH_WEST: Glyph = Glyph(Direction::South, Direction::West, '7');
-const SOUTH_EAST: Glyph = Glyph(Direction::South, Direction::East, 'F');
-const START: Glyph = Glyph(Direction::None, Direction::None, 'S');
-const LAND: Glyph = Glyph(Direction::None, Direction::None, '.');
+const NORTH_SOUTH: Glyph = Glyph {
+    directions: (Direction::North, Direction::South),
+    c: '|',
+};
+const EAST_WEST: Glyph = Glyph {
+    directions: (Direction::East, Direction::West),
+    c: '-',
+};
+const NORTH_EAST: Glyph = Glyph {
+    directions: (Direction::North, Direction::East),
+    c: 'L',
+};
+const NORTH_WEST: Glyph = Glyph {
+    directions: (Direction::North, Direction::West),
+    c: 'J',
+};
+const SOUTH_WEST: Glyph = Glyph {
+    directions: (Direction::South, Direction::West),
+    c: '7',
+};
+const SOUTH_EAST: Glyph = Glyph {
+    directions: (Direction::South, Direction::East),
+    c: 'F',
+};
+const START: Glyph = Glyph {
+    directions: (Direction::Start, Direction::Start),
+    c: 'S',
+};
+const LAND: Glyph = Glyph {
+    directions: (Direction::None, Direction::None),
+    c: '.',
+};
 
 fn char_to_glyph(c: char) -> Glyph {
     match c {
@@ -46,105 +74,140 @@ fn main() {
     let mut input = String::new();
     std::io::stdin().lock().read_to_string(&mut input).unwrap();
     let lines = input.lines().clone();
-    let _s = find_start_point(&lines);
+    let start_point = find_start_point(&lines);
+    println!("Start Point: {:?}", start_point);
+    let mid_point = traverse_map_from_start(start_point, lines) / 2;
+    println!("{:?}", mid_point); // 6931
 }
 
-fn find_start_point(lines: &Lines) -> (usize, usize) {
+fn find_start_point(lines: &Lines) -> Point {
     let i: Vec<(usize, usize)> = lines
         .clone()
         .enumerate()
         .filter(|(_, l)| l.contains('S'))
         .map(|(i, l)| (l.find('S').unwrap(), i))
         .collect();
-    i[0]
+
+    Point {
+        x: i[0].0,
+        y: i[0].1,
+        glyph: START,
+    }
 }
 
-fn traverse_map_from_start(start: (usize, usize), lines: Lines) -> usize {
-    // let x: &str = lines.collect::<Vec<&str>>()[start.1];
-    let s = find_next_point(start.0, start.1, lines);
-    // println!("{}", char_for_point(start, &lines));
-    0
+fn traverse_map_from_start(start: Point, lines: Lines) -> usize {
+    let mut np = start;
+    let mut pp: Option<Point> = None;
+    let mut count = 0;
+    loop {
+        count += 1;
+        let rp = find_next_point(np, lines.clone(), pp);
+        match rp {
+            Some(p) => {
+                println!("{}", np.glyph.c);
+                pp = Some(np);
+                np = p;
+            }
+            None => {
+                println!("No more points");
+                break;
+            }
+        }
+    }
+    count
 }
 
-fn find_next_point(x: usize, y: usize, lines: Lines) -> (usize, usize) {
+fn find_next_point(point: Point, lines: Lines, previous_point: Option<Point>) -> Option<Point> {
     let mut connected_points: Vec<Point> = Vec::new();
-    let current_char = point_for_point((Some(x), Some(y)), &lines);
 
-    let top = point_for_point((Some(x), y.checked_sub(1)), &lines);
-    let left = point_for_point((x.checked_sub(1), Some(y)), &lines);
-    let right = point_for_point((Some(x + 1), Some(y)), &lines);
-    let bottom = point_for_point((Some(x), Some(y + 1)), &lines);
+    let top = point_for_point((Some(point.x), point.y.checked_sub(1)), &lines);
+    let left = point_for_point((point.x.checked_sub(1), Some(point.y)), &lines);
+    let right = point_for_point((Some(point.x + 1), Some(point.y)), &lines);
+    let bottom = point_for_point((Some(point.x), Some(point.y + 1)), &lines);
 
     if top.is_some() {
-        let tc = top.unwrap();
-        let g = char_to_glyph(tc.glyph.2);
-        match g.0 {
-            Direction::South => connected_points.push(tc),
-            _ => (),
-        }
-        // match g.1 {
-        //     Direction::North => connected_points.push(tc),
-        //     _ => (),
-        // }
-    }
+        let t = top.unwrap();
 
-    if left.is_some() {
-        let lc = left.unwrap();
-        let g = char_to_glyph(lc.glyph.2);
-        match g.0 {
-            Direction::East => connected_points.push(lc),
-            _ => (),
+        if (t.glyph.directions.1 == Direction::South || t.glyph.directions.0 == Direction::South)
+            && (point.glyph.directions.0 == Direction::North
+                || point.glyph.directions.1 == Direction::North
+                || point.glyph.directions.0 == Direction::Start)
+        {
+            connected_points.push(t);
         }
-        // match g.1 {
-        //     Direction::West => connected_points.push(lc),
-        //     _ => (),
-        // }
     }
-
     if right.is_some() {
-        let rc = right.unwrap();
-        let g = char_to_glyph(rc.glyph.2);
-        // match g.0 {
-        //     Direction::East => connected_points.push(rc),
-        //     _ => (),
-        // }
-        match g.1 {
-            Direction::West => connected_points.push(rc),
-            _ => (),
+        let t = right.unwrap();
+        if (t.glyph.directions.1 == Direction::West || t.glyph.directions.0 == Direction::West)
+            && (point.glyph.directions.0 == Direction::East
+                || point.glyph.directions.1 == Direction::East
+                || point.glyph.directions.0 == Direction::Start)
+        {
+            connected_points.push(t)
         }
     }
 
     if bottom.is_some() {
-        let bc = bottom.unwrap();
-        let g = char_to_glyph(bc.glyph.2);
-        match g.0 {
-            Direction::North => connected_points.push(bc),
-            _ => (),
+        let t = bottom.unwrap();
+        if (t.glyph.directions.1 == Direction::North || t.glyph.directions.0 == Direction::North)
+            && (point.glyph.directions.0 == Direction::South
+                || point.glyph.directions.1 == Direction::South
+                || point.glyph.directions.0 == Direction::Start)
+        {
+            connected_points.push(t)
+        }
+    }
+    if left.is_some() {
+        let t = left.unwrap();
+        if (t.glyph.directions.1 == Direction::East || t.glyph.directions.0 == Direction::East)
+            && (point.glyph.directions.0 == Direction::West
+                || point.glyph.directions.1 == Direction::West
+                || point.glyph.directions.0 == Direction::Start)
+        {
+            connected_points.push(t)
         }
     }
 
-    // let surrounding_chars: Vec<char> = vec![top, right, bottom, left]
-    //     .iter()
-    //     .filter(|s| s.is_some())
-    //     .map(|f| f.unwrap())
-    //     .collect();
-    // for c in surrounding_chars {
-    //     let g = char_to_glyph(&c);
-    // }
-
-    println!("Connected Points {:?}", connected_points);
-
-    // println!("{:?} : {:?} : {:?} : {:?}", top, right, bottom, left);
-    (0, 0)
+    match previous_point {
+        Some(pp) => {
+            let p: Vec<&Point> = connected_points
+                .iter()
+                .filter(|cp| {
+                    let z = **cp;
+                    z.x != pp.x || z.y != pp.y
+                })
+                .collect();
+            if p.len() == 1 && p[0].glyph.c == 'S' {
+                Some(p[0].to_owned())
+            } else if p.is_empty() {
+                None
+            } else {
+                Some(
+                    *p.iter()
+                        .find(|i| i.glyph.directions.0 != Direction::Start)
+                        .unwrap()
+                        .to_owned(),
+                )
+            }
+        }
+        None => Some(connected_points[0]),
+    }
 }
 
 fn point_for_point(point: (Option<usize>, Option<usize>), lines: &Lines) -> Option<Point> {
     match point.1 {
         Some(x) => {
+            if x >= lines.to_owned().collect::<Vec<&str>>().len() {
+                return None;
+            }
             let line = lines.to_owned().collect::<Vec<&str>>()[x];
             match point.0 {
                 Some(y) => {
-                    let c = line.chars().nth(y).unwrap().clone();
+                    let len = line.chars().collect::<Vec<char>>().len();
+                    if y >= len {
+                        return None;
+                    }
+                    let c = line.chars().nth(y).unwrap();
                     let g = char_to_glyph(c);
                     Some(Point {
                         x: point.0.unwrap(),
@@ -163,18 +226,37 @@ fn point_for_point(point: (Option<usize>, Option<usize>), lines: &Lines) -> Opti
 mod tests {
     use crate::{find_start_point, traverse_map_from_start};
 
-    const INPUT: &str = "..F7.
-.FJ|.
-SJ.L7
-|F--J
-LJ...";
+    const INPUT1: &str = "
+F-7F---7
+|.|||J||
+|.LJJ.L|
+|..F-S-J
+L--J....";
 
     #[test]
     fn test_part1() {
-        let lines = INPUT.lines();
+        let lines = INPUT1.lines();
         let start_point = find_start_point(&lines);
-        println!("{:?}", start_point);
-        let mid_point = traverse_map_from_start(start_point, lines);
-        assert_eq!(mid_point, 8);
+        let mid_point = traverse_map_from_start(start_point, lines) / 2;
+        assert_eq!(mid_point, 13);
+    }
+
+    const INPUT2: &str = "FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L";
+
+    #[test]
+    fn test_part2() {
+        let lines = INPUT2.lines();
+        let start_point = find_start_point(&lines);
+        let mid_point = traverse_map_from_start(start_point, lines) / 2;
+        assert_eq!(mid_point, 80);
     }
 }
